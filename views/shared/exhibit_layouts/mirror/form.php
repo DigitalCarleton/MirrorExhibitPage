ecc359b7ad595f51c2a98b1f1c7305394dcfb643
padillac <shinimaninima@gmail.com>
2020-05-13 15:07:19 -0400

    commented out menu and added temporary help text for finding page IDs.

--- views/shared/exhibit_layouts/mirror/form.php
+++ views/shared/exhibit_layouts/mirror/form.php
@@ -1,52 +1,53 @@
 <?php // This code chunk creates the mirror exhibit page selection menu ?>
-<!-- <div class="selected-items">
+<div class="selected-items">
     <h4><?php echo __('Select Page to Mirror'); ?></h4>
 
     <?php
     $formStem = $block->getFormStem();
     $db = get_db();
-    $pagesTable = $db->getTable("ExhibitPage");
-    $pages = $pagesTable->fetchObjects("SELECT * FROM omeka_exhibit_pages");
+    // BELOW TEMPORARILY REMOVED SO PLUGIN CAN BE USED WHILE MENU IS BEING WORKED ON
+    // $pagesTable = $db->getTable("ExhibitPage");
+    // $pages = $pagesTable->fetchObjects("SELECT * FROM omeka_exhibit_pages");
 
-    echo "<div id='exhibit-page-display' class='selected-item-list'>";
+    // echo "<div id='exhibit-page-display' class='selected-item-list'>";
 
-    foreach ($pages as $index => $exhibitPage) {
-        // Get page data for menu
-        $pageTitle = $exhibitPage->title;
-        $pageID = $exhibitPage->id;
-        $imageURL = "";
+    // foreach ($pages as $index => $exhibitPage) {
+    //     // Get page data for menu
+    //     $pageTitle = $exhibitPage->title;
+    //     $pageID = $exhibitPage->id;
+    //     $imageURL = "";
 
-        $pageAttachments = $exhibitPage->getAllAttachments();
-        if (count($pageAttachments) > 0) {
-            $fileAttachments = $pageAttachments[0]->getItem()->getFiles();
-            if (count($fileAttachments) > 0 && $fileAttachments[0]->hasThumbnail()) {
-                $imageURL = $fileAttachments[0]->getWebPath();
-            }
-        }
+    //     $pageAttachments = $exhibitPage->getAllAttachments();
+    //     if (count($pageAttachments) > 0) {
+    //         $fileAttachments = $pageAttachments[0]->getItem()->getFiles();
+    //         if (count($fileAttachments) > 0 && $fileAttachments[0]->hasThumbnail()) {
+    //             $imageURL = $fileAttachments[0]->getWebPath();
+    //         }
+    //     }
 
-        // Create menu element with page data
-        echo "
-        <div class='attachment' data-attachment-index='{$index}' onclick=\"document.getElementsByName('{$formStem}[text]')[0].value = {$pageID};\">
-            <div class='attachment-body'>
-                <div class='attachment-background' style=\"background: url('{$imageURL}') center / cover\"></div>
-                <h5>{$pageTitle}<br>Page ID: {$pageID}</h5>
-            </div>
-        </div>
-        ";
-    }
+    //     // Create menu element with page data
+    //     echo "
+    //     <div class='attachment' data-attachment-index='{$index}' onclick=\"document.getElementsByName('{$formStem}[text]')[0].value = {$pageID};\">
+    //         <div class='attachment-body'>
+    //             <div class='attachment-background' style=\"background: url('{$imageURL}') center / cover\"></div>
+    //             <h5>{$pageTitle}<br>Page ID: {$pageID}</h5>
+    //         </div>
+    //     </div>
+    //     ";
+    // }
 
-    // Styles for page selection menu
-    echo "</div>
-    <style>
-    #exhibit-page-display {
-        overflow-y: scroll;
-        height: 30vh;;
-        margin-bottom: 1vh;
-    }
-    </style>
-    ";
+    // // Styles for page selection menu
+    // echo "</div>
+    // <style>
+    // #exhibit-page-display {
+    //     overflow-y: scroll;
+    //     height: 30vh;;
+    //     margin-bottom: 1vh;
+    // }
+    // </style>
+    // ";
     ?>
-</div> -->
+</div>
 
 <?php // Temporary help text explaining how to get ID from URL ?>
     <p>Find the ID of the exhibit page you want to mirror by navigating to the exhibit page on the admin side, and taking the number at the end of the URL.</p>
