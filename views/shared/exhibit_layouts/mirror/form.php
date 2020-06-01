b1c2dcd25f9de39b408e8338037506e04134f061
padillac <shinimaninima@gmail.com>
2020-06-01 15:39:22 -0400

    cleaning code

--- views/shared/exhibit_layouts/mirror/form.php
+++ views/shared/exhibit_layouts/mirror/form.php
@@ -1,12 +1,10 @@
 <?php // This code chunk creates the mirror exhibit page selection menu
 $formStem = $block->getFormStem();
 $db = get_db();
-// set_time_limit(0);
 ?>
 
 
 <?php # SELECT EXHIBIT ?>
-
 <div>
 <h4>Select an Exhibit</h4>
 
@@ -26,7 +24,6 @@ echo "</datalist>";
 ?>
 </div>
 
-
 <script>
 function submenuSelector() {
     var slug = document.getElementById('exhibit-selector').value;
@@ -44,9 +41,7 @@ function submenuSelector() {
 
 
 
-
 <?php # SELECT PAGE FROM EXHIBIT ?>
-
 <div class='selected-items'><h4>Select a Page to Mirror</h4>
 <div id='exhibit-page-display' class='selected-item-list'>
 
@@ -54,57 +49,48 @@ function submenuSelector() {
 $pagesTable = $db->getTable("ExhibitPage");
 $allPages = $pagesTable->fetchObjects("SELECT * FROM omeka_exhibit_pages");
 
-########################################################## Create sub menu of pages for each exhibit
 foreach ($exhibits as $index => $exhibit) {
     $slug = $exhibit['slug'];
     $id = $exhibit['id'];
     echo "<div class='pages-sub-menu' id='{$slug}' hidden>";
 
-    $pages = array();
     foreach($allPages as $index => $page) {
         if ($page['exhibit_id'] == $id) {
-            $pages[] = $page;
+            // Get page data for menu
+            $pageTitle = $exhibitPage->title;
+            $pageID = $exhibitPage->id;
+            $imageURL = "";
+            
+            // THIS SECTION FINDS BACKGROUND IMAGES FOR ALL MENU ELEMENTS -- VERY SLOW
+            // try {
+            //     $pageAttachments = $exhibitPage->getAllAttachments();
+            //     if (count($pageAttachments) > 0) {
+            //         $item = $pageAttachments[0]->getItem();
+            //         if ($item) {
+            //             $fileAttachments = $item->getFiles();
+            //             if (count($fileAttachments) > 0 && $fileAttachments[0]->hasThumbnail()) {
+            //                 $imageURL = $fileAttachments[0]->getWebPath();
+            //             }
+            //         }
+            //     }
+            // } catch (Exception $e) {
+            //     $imageURL = "";
+            // }
+
+
+            // Create menu element with page data
+            echo "
+            <div class='attachment' data-attachment-index='{$index}' onclick=\"document.getElementsByName('{$formStem}[text]')[0].value = {$pageID};\">
+                <div class='attachment-body'>
+                    <div class='attachment-background' style=\"background: url('{$imageURL}') center / cover\"></div>
+                    <h5>{$pageTitle}<br>Page ID: {$pageID}</h5>
+                </div>
+            </div>
+            ";
         }
     }
-
-    foreach ($pages as $index => $exhibitPage) {
-        // Get page data for menu
-        $pageTitle = $exhibitPage->title;
-        $pageID = $exhibitPage->id;
-        $imageURL = "";
-        
-        // try {
-        //     $imageURL = "";
-        //     $pageAttachments = $exhibitPage->getAllAttachments();
-        //     if (count($pageAttachments) > 0) {
-        //         $item = $pageAttachments[0]->getItem();
-        //         if ($item) {
-        //             $fileAttachments = $item->getFiles();
-        //             if (count($fileAttachments) > 0 && $fileAttachments[0]->hasThumbnail()) {
-        //                 $imageURL = $fileAttachments[0]->getWebPath();
-        //             }
-        //         }
-        //     }
-        // } catch (Exception $e) {
-        //     $imageURL = "";
-        // }
-
-        // Create menu element with page data
-        echo "
-        <div class='attachment' data-attachment-index='{$index}' onclick=\"document.getElementsByName('{$formStem}[text]')[0].value = {$pageID};\">
-            <div class='attachment-body'>
-                <div class='attachment-background' style=\"background: url('{$imageURL}') center / cover\"></div>
-                <h5>{$pageTitle}<br>Page ID: {$pageID}</h5>
-            </div>
-        </div>
-        ";
-    }
-
     echo "</div>";
-
-
 }
-#################################################################
 
 // Styles for page selection menu
 echo "</div>
@@ -118,14 +104,6 @@ echo "</div>
 ";
 ?>
 </div>
-
-
-
-
-<!-- <?php // Temporary help text explaining how to get ID from URL ?>
-    <p>Find the ID of the exhibit page you want to mirror by navigating to the exhibit page on the admin side, and taking the number at the end of the URL.</p>
-    <p>For example: https://www.myomekasite.com/admin/exhibits/edit-page/6 the ID for this page would be 6.</p>
-    <p>Insert this ID in the textbox below to mirror the page.</p> -->
 
 
 
